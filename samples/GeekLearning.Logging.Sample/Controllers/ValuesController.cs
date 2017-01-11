462d0570fb7e72901140a584f70ca4799df1fce6
Galo <adrien@siffermann.me>
2017-01-11 17:34:50 +0100

    .NET Core 1.1 Migration (#7)
    * .NET Core 1.1 Migration

--- samples/GeekLearning.Logging.Sample/Controllers/ValuesController.cs
+++ samples/GeekLearning.Logging.Sample/Controllers/ValuesController.cs
@@ -1,12 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-
-namespace GeekLearning.Logging.Sample.Controllers
+﻿namespace GeekLearning.Logging.Sample.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Extensions.Logging;
+    using System;
+    using System.Collections.Generic;
+
     [Route("api/[controller]")]
     public class ValuesController : Controller
     {
@@ -17,7 +15,6 @@ namespace GeekLearning.Logging.Sample.Controllers
             this.logger = logger;
         }
 
-        // GET api/values
         [HttpGet]
         public IEnumerable<string> Get()
         {
@@ -26,26 +23,22 @@ namespace GeekLearning.Logging.Sample.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET api/values/5
         [HttpGet("{id}")]
         public string Get(int id)
         {
             throw new Exception("une erreur");
         }
 
-        // POST api/values
         [HttpPost]
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT api/values/5
         [HttpPut("{id}")]
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE api/values/5
         [HttpDelete("{id}")]
         public void Delete(int id)
         {
--- src/GeekLearning.Logging/StackTrace/StackTraceParameter.cs
+++ src/GeekLearning.Logging/StackTrace/StackTraceParameter.cs
@@ -1,13 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace GeekLearning.Logging.StackTrace
+﻿namespace GeekLearning.Logging.StackTrace
 {
     public class StackTraceParameter
     {
         public StackTraceToken Name { get; internal set; }
+
         public StackTraceToken Type { get; internal set; }
     }
 }
